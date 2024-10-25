8571aeca7c3307a6d7abde0d92af8c891abe60ec
Mariana <mariana_penagos04192@elpoli.edu.co>
2024-10-24 23:39:20 -0500

    update

--- backend/controller/pagesController.js
+++ backend/controller/pagesController.js
@@ -5,39 +5,29 @@ function showPets(req, res) {
   res.sendFile(path.join(proyectPath,'frontend','modules','pets','showPets.html'));
 }
 
-
 function showVaccineRecords(req, res) {
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','pets','showVaccineRecords.html'));
 }
-
 
 function addUser(req, res){
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','admin','addUsers.html'));
 }
 
-
 function createMedicine(req, res){
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','adminDashboard','inventoryManagement','addMedicine.html'));
 }
-
 
 function showAppointment(req, res) {
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','adminDashboard','adminAppointments', 'adminEditAppointment.html'));
 }
 
-
 function showAppointmentCrt(req, res) {
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','adminDashboard','adminAppointments', 'adminCreateAppointment.html'));
-}
-
-function showAppointmentn(req, res) {
-  const proyectPath = path.resolve(__dirname, '../..');
-  res.sendFile(path.join(proyectPath,'frontend','modules','adminDashboard','adminAppointments'));
 }
 
 module.exports = { 
