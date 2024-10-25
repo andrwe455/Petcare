b18de281f0eff898a0cc640522b6a6fa9adaf017
Mariana <mariana_penagos04192@elpoli.edu.co>
2024-10-24 23:28:25 -0500

    update

--- backend/controller/pagesController.js
+++ backend/controller/pagesController.js
@@ -5,25 +5,30 @@ function showPets(req, res) {
   res.sendFile(path.join(proyectPath,'frontend','modules','pets','showPets.html'));
 }
 
+
 function showVaccineRecords(req, res) {
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','pets','showVaccineRecords.html'));
 }
+
 
 function addUser(req, res){
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','admin','addUsers.html'));
 }
 
+
 function createMedicine(req, res){
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','adminDashboard','inventoryManagement','addMedicine.html'));
 }
 
+
 function showAppointment(req, res) {
   const proyectPath = path.resolve(__dirname, '../..');
   res.sendFile(path.join(proyectPath,'frontend','modules','adminDashboard','adminAppointments', 'adminEditAppointment.html'));
 }
+
 
 function showAppointmentCrt(req, res) {
   const proyectPath = path.resolve(__dirname, '../..');
