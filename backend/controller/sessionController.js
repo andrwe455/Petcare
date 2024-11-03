63483ee6fbe95375f4c695585f17b88cb4b6f21f
andrwe455 <andrwe455@gmail.com>
2024-11-03 09:44:18 -0500

    HU-31, HU-32, fix:
    feat login and logout are implemented succesfully, update pets vaccine registry fixed

--- backend/controller/sessionController.js
+++ backend/controller/sessionController.js
@@ -1,10 +1,35 @@
-const session = require("express-session")
+function getUserId(req,res, next) {
+   let session = req.session.user   
+  
+   if (session) {
+      req.user = {};
+      req.user.role = session.role;
+      req.user.id = session._id;
+      next();
+   }
+   else {
+       res.redirect('/login');
+   }
+}
 
-
-function getUserId(req) {
-    return req.session.userId
+function roleValidator(req,res,next){
+    let session = req.session.user;
+    console.log(session);
+    if(session){
+        let path = req.path;
+        let role = session.role;
+        path = path.split('/');
+        path = path[2];
+        if(path === role){
+            next();
+        }else{
+            res.redirect(`/home/${role}`+`?error=`+`You don't have permission to access this page`);
+        }
+    }else{
+        res.redirect('/login');
+    }
 }
 
 
-module.exports = { getUserId }
+module.exports = { getUserId, roleValidator };
 
